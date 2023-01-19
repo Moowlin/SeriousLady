13ca39f47252dc146081e0c319e040a5055e0e0f
pupil <pupil@ivkhk.ee>
2023-01-19 12:34:43 +0200

    добавлена функция печати в csv файл

--- TFS-11.NO.py
+++ TFS-11.NO.py
@@ -6,17 +6,22 @@ class Product_base:
         self.id = st1
         self.name = st2
         self.price = st3
+        self.dict = {'id': self.id, 'name': self.name, 'price': self.price}
 
     def printInfo(self):
         print("id: ", self.id)
         print("name: ", self.name)
         print("price: ", self.price)
 
+    def return_dict(self):
+        return self.dict
+
 
 class Product_property4(Product_base):
     def __init__(self, st1, st2, st3, st4):
         Product_base.__init__(self, st1, st2, st3)
         self.vendercode = st4
+        self.dict = {'id': self.id, 'name': self.name, 'price': self.price, 'vendercode': self.vendercode}
 
     def printInfo(self):
         Product_base.printInfo(self)
@@ -27,37 +32,41 @@ class Product_property5(Product_property4):
     def __init__(self, st1, st2, st3, st4, st5):
         Product_property4.__init__(self, st1, st2, st3, st4)
         self.quantity = st5
+        self.dict = {'id': self.id, 'name': self.name, 'price': self.price, 'vendercode': self.vendercode, 'quantity': self.quantity}
 
     def printInfo(self):
         Product_property4.printInfo(self)
         print("quantity: ", self.quantity)
 
 massiv_products = []
+clmnq = None
+
 
 def create_massiv_products(product):
     global massiv_products
     massiv_products.append(product)
-    print(massiv_products)
+
 
 product = None
 def create_product():
     global product
+    global clmnq
     clmnq = int(input('укажи число: '))
     if clmnq == 3:
         product = Product_base(2342, "sdfsf", "wfwaqf")
         #product = Product_base(id.get(), name.get(), price.get())
-        product.printInfo()
+        #product.printInfo()
         create_massiv_products(product)
     elif clmnq == 4:
         product = Product_property4(456, 'второе св-во', '3 сво-во', "4 св-во")
         #product = Product_property4(id.get(), name.get(), price.get(), vendercode.get())
-        product.printInfo()
+        #product.printInfo()
         create_massiv_products(product)
     elif clmnq == 5:
         product = Product_property5(65745, 'второе св-во', '3 сво-во', "4 св-во", "5 сво-во")
         #product = Product_property5(id.get(), name.get(), price.get(), vendercode.get(), quantity.get())
-        product.printInfo()
-        #create_massiv_products(product)
+        #product.printInfo()
+        create_massiv_products(product)
     else:
         print("doesn't work")
 
@@ -68,26 +77,38 @@ def button_count_products():
     for i in range(x):
         create_product()
 
+button_count_products()
 
-#button_count_products()
-
-create_product()
-print(product)
-
-print((type(product)))
-print(dir(product))
-
-
-
-# --------------- Обработка массива
-#for i in range(len(massiv_products)):
-#    for values, key in massiv_products[i].dict.items():
-#        print(values, key)
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 
+print()
+
+products_csv = []
+for i in range(len(massiv_products)):
+    products_csv.append(massiv_products[i].return_dict())
+    print()
+
+print(products_csv)
 
 file_name = "products.csv"
 
+name_columns = ['id', 'name', 'price', 'vendercode', 'quantity']
+colums_csv = []
 
-# https://qna.habr.com/q/967195
+for i in range(clmnq):
+    colums_csv.append(name_columns[i])
+
+
+
+with open(file_name, 'w', newline='') as file:
+    wrinter = csv.DictWriter(file, fieldnames=colums_csv, delimiter=';')
+    wrinter.writeheader()
+
+    wrinter.writerows(products_csv)
+
